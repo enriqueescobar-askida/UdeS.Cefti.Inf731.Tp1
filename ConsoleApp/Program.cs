8bcb11e12febfef87ff2a70f9e611d0ebc2aa920
Enrique Escobar <enrique.escobar@axon-id.com>
2017-03-09 12:30:50 -0500

    update

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,6 +1,7 @@
 ﻿namespace ConsoleApp
 {
     using System;
+    using System.Collections;
     using System.Collections.Generic;
     using System.IO;
     using System.Text;
@@ -15,6 +16,29 @@
             Console.Out.WriteLine(dr.ToString());
             dr.ReadFile();
             Console.In.ReadLine();
+
+            // Creates and initializes a new SortedList.
+            SortedList mySL = new SortedList();
+            mySL.Add("Third", "!");
+            mySL.Add("Second", "World");
+            mySL.Add("First", "Hello");
+
+            // Displays the properties and values of the SortedList.
+            Console.WriteLine("mySL");
+            Console.WriteLine("  Count:    {0}", mySL.Count);
+            Console.WriteLine("  Capacity: {0}", mySL.Capacity);
+            Console.WriteLine("  Keys and Values:");
+            PrintKeysAndValues(mySL);
+            Console.In.ReadLine();
+        }
+        public static void PrintKeysAndValues(SortedList myList)
+        {
+            Console.WriteLine("\t-KEY-\t-VALUE-");
+            for (int i = 0; i < myList.Count; i++)
+            {
+                Console.WriteLine("\t{0}:\t{1}", myList.GetKey(i), myList.GetByIndex(i));
+            }
+            Console.WriteLine();
         }
     }
 }
